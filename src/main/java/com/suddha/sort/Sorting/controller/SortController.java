b7b717675ddec85e251ed42384171b223aa7db59
Suddhaswatta <suddhaswatta.maity1@gmail.com>
2021-09-11 23:35:57 +0530

    Flux Return

--- src/main/java/com/suddha/sort/Sorting/controller/SortController.java
+++ src/main/java/com/suddha/sort/Sorting/controller/SortController.java
@@ -3,9 +3,10 @@ package com.suddha.sort.Sorting.controller;
 import com.suddha.sort.Sorting.service.SortFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.Collections;
-import java.util.List;
 import java.util.stream.IntStream;
 
 @RestController
@@ -16,28 +17,31 @@ public class SortController {
     SortFactory factory;
 
 
-    @GetMapping("/insertion-sort")
-    public List<String> triggerInsertionSort(){
+    @GetMapping("/insertion-sort/{n}")
+    public Flux<String> triggerInsertionSort(@PathVariable int n){
 
-        Integer[] arr = IntStream.range(0, 8)
+        Integer[] arr = IntStream.range(0, n)
                 .boxed()
                 .sorted(Collections.reverseOrder())
                 .toArray(Integer[]::new);
 
-        System.out.println();
-
-        return factory.getSortingAlgorithm("INSERTION_SORT").sort(arr);
+        return Flux
+                .fromIterable(
+                        factory.getSortingAlgorithm("INSERTION_SORT").sort(arr)
+                ).delayElements(Duration.ofSeconds(1));
     }
 
-    @GetMapping("/shell-sort")
-    public List<String> shellSort(){
+    @GetMapping("/shell-sort/{n}")
+    public Flux<String> shellSort(@PathVariable int n){
 
-        Integer[] arr = IntStream.range(0, 8)
+        Integer[] arr = IntStream.range(0, n)
                 .boxed()
                 .sorted(Collections.reverseOrder())
                 .toArray(Integer[]::new);
 
-        return factory.getSortingAlgorithm("SHELL_SORT").sort(arr);
+        return Flux.fromIterable(
+                factory.getSortingAlgorithm("SHELL_SORT").sort(arr)
+        ).delayElements(Duration.ofSeconds(1));
     }
 
 
